75fda7a5015930a082a3def6ffea3e01621f6bca
Rishav raj <rishi9520raj@gmail.com>
2026-04-23 21:14:11 +0530

    improved

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <div className='bg-black text-white text-center py-4'>
       <p>© 2026 GitHub Profile App</p>
       <p className='text-sm text-gray-400'>Made with ❤️ by Krishna</p>
+       <p className='text-sm text-gray-400'>Improved with ❤️ by Rishav Raj</p>
     </div>
   )
 }
